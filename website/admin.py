2cdee89dbab791c68fcb8088a64272d1885aa70f
realMrBhandari <23021086@geu.ac.in>
2026-09-15 14:42:33 +0530

    feat: implemented admin console

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ExperimentMetadata
+
+
+@admin.register(ExperimentMetadata)
+class ExperimentMetadataAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "experiment_id",
+        "user_email",
+        "created_at",
+        "experiment_status",
+        "started_at",
+        "completed_at",
+    )
+
+    list_filter = ("experiment_status",)
+
+    search_fields = (
+        "experiment_id",
+        "user_email",
+    )
+
+    ordering = ("-created_at",)
+
+    readonly_fields = (
+        "experiment_id",
+        "user_email",
+        "created_at",
+        "experiment_status",
+        "started_at",
+        "completed_at",
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
