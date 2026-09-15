98de2550133c29ca092554c795ad5035c0085ebb
realMrBhandari <23021086@geu.ac.in>
2026-09-15 12:44:08 +0530

    feat: implemented email service in website for notifying user's once their experiment has been queued

---
+++ website/mailer.py
@@ -0,0 +1,24 @@
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def notify_user_queued(experiment):
+    message = f"""Greetings,
+
+Your molecular docking experiment has been successfully submitted and is queued for processing. Your Experiment ID is  {experiment.experiment_id}
+
+You can track your experiment's progress using the following link:
+https://dockinity.com/docking-results/{experiment.experiment_id}
+
+Regards,
+Dockinity
+
+"""
+
+    send_mail(
+        subject="Experiment received",
+        message=message,
+        from_email=f"Dockinity <{settings.DEFAULT_FROM_EMAIL}>",
+        recipient_list=[experiment.user_email],
+        fail_silently=True,
+    )
